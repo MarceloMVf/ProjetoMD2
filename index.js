03d9c6fca22f2ee1afbf122b556c5b8fb0ef127a
MarceloMVf <marcelomvf37@gmail.com>
2021-09-27 22:49:50 -0300

    Secury

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 const express = require('express');
 const app = express();
+const port = 3000;
+const path = require('path');
+
+app.set("view engine", "ejs")
+
+app.use(express.static(path.join(__dirname, "public"))); // 
 
 app.get('/', (req, res) => {
     res.send("hello blusds")
 })
 
 
-app.get('/teste2', (req, res) => {
-    res.send("Bungasdasdasdaaar")
+app.get('/page', (req, res) => {
+    res.render('pagina')
 })
 
 app.get('/teste3', (req, res) => {
     res.send("Quero jogaaar")
 })
 
-app.listen(3000)
+app.listen(port, () => console.log(`servidor rodando na porta ${port}`))
