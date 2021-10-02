7a7f8fc520a6af5366b43d8cd70d196f727f71bf
MarceloMVf <marcelomvf37@gmail.com>
2021-10-02 20:00:38 -0300

    confirm

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ app.set("view engine", "ejs")
 app.use(express.static(path.join(__dirname, "public"))); // 
 app.use(express.urlencoded());
 
-app.get("/cadastro", (req, res, next) =>{
-    res.render("cadastro")
-})
+app.get("/", function (req, res) {
+    res.send("Hello World");
+  });
 
 app.listen(port, () => console.log(`servidor rodando na porta ${port}`))
